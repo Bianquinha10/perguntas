87b34cee7539dc15f7defdb157b7da060609af64
Bianca Esser Zeferino <127758555+Bianquinha10@users.noreply.github.com>
2024-09-12 09:55:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+{
+    enunciado: "Pergunta 2",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+}
+{
+    enunciado: "Pergunta 3",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+}
+{
+    enunciado: "Pergunta 4",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+}
+enunciado: "Pergunta 5",
+        alternativas:[
+            "alternativa 1",
+            "alternativa 2"
+}
+}
+]
